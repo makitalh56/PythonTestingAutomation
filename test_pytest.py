0b6878c1538fbe2c533c8ec39db956bee307c638
Hanna <hanna.friman97@gmail.com>
2023-11-29 11:29:21 +0200

    Pytest file for testing calc.py

---
+++ test_pytest.py
@@ -0,0 +1,14 @@
+from calc import add, multiply, power
+
+def test_add():
+    assert add(1,1) == 2
+    assert add(-3,1) == -2
+
+def test_multiply():
+    assert multiply(2,2) == 4
+    assert multiply(-3,1) == -3
+    
+def test_power():
+    assert power(1,1) == 1
+    assert power(2,4) == 16
+    
