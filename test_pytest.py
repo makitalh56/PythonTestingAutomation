6669a72e30fb0af26d18827de502edb9931f6733
Hanna <hanna.friman97@gmail.com>
2023-12-07 13:18:13 +0200

    Add one sum test

--- test_pytest.py
+++ test_pytest.py
@@ -4,12 +4,13 @@ from calc import add, multiply, power
 def test_add():
     assert add(1, 1) == 2
     assert add(-3, 1) == -2
+    assert add(-1, -2) == -3
 
 
 def test_multiply():
     assert multiply(2, 2) == 4
     assert multiply(-3, 1) == -3
-    assert multiply(-1,-3)  == 3
+    assert multiply(-1, -3)  == 3
 
 
 def test_power():
